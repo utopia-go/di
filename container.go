82ae0a68888e05e9d7c55511c195b2565be2aa4e
Engibeer <github@engi.beer>
2024-07-21 20:43:21 -0400

    Tests: Adding tests

--- container.go
+++ container.go
@@ -6,75 +6,77 @@ import (
 )
 
 type Container struct {
-	dependencies map[string]Dependency
-	instances    map[string]interface{}
+	Dependencies map[string]Dependency
+	Instances    map[string]interface{}
 }
 
 func NewContainer() *Container {
 	container := &Container{}
+	container.Dependencies = make(map[string]Dependency)
+	container.Instances = make(map[string]interface{})
 
-	di := Dependency{name: "di", callback: func(params ...interface{}) interface{} { return container }}
+	di := Dependency{Name: "di", Callback: func(params ...interface{}) interface{} { return container }}
 
-	container.dependencies[di.name] = di
+	container.Dependencies[di.Name] = di
 
 	return container
 }
 
 func (container *Container) Set(dependency Dependency) *Container {
-	if dependency.name != "di" {
-		if container.instances[dependency.name] != "" {
-			delete(container.instances, dependency.name)
+	if dependency.Name != "di" {
+		if container.Instances[dependency.Name] != "" {
+			delete(container.Instances, dependency.Name)
 		}
 
-		container.dependencies[dependency.name] = dependency
+		container.Dependencies[dependency.Name] = dependency
 	}
 
 	return container
 }
 
 func (container *Container) Inject(dependency Dependency, fresh bool) (interface{}, error) {
-	if container.instances[dependency.name] != "" && !fresh {
-		return container.instances[dependency.name], nil
+	if _, ok := container.Instances[dependency.Name]; ok && !fresh {
+		return container.Instances[dependency.Name], nil
 	}
 
 	var arguments []interface{}
 
-	for i := 0; i < len(dependency.dependencies); i++ {
-		if _, ok := container.instances[dependency.dependencies[i]]; ok {
-			arguments = append(arguments, container.instances[dependency.dependencies[i]])
+	for i := 0; i < len(dependency.Dependencies); i++ {
+		if _, ok := container.Instances[dependency.Dependencies[i]]; ok {
+			arguments = append(arguments, container.Instances[dependency.Dependencies[i]])
 			continue
 		}
 
-		if _, ok := container.dependencies[dependency.dependencies[i]]; !ok {
-			return container.instances[dependency.dependencies[i]], errors.New(fmt.Sprintf("Failed to find dependency %s", dependency.dependencies[i]))
+		if _, ok := container.Dependencies[dependency.Dependencies[i]]; !ok {
+			return container.Instances[dependency.Dependencies[i]], errors.New(fmt.Sprintf("Failed to find dependency %s", dependency.Dependencies[i]))
 		}
-		d, _ := container.Get(dependency.dependencies[i])
+		d, _ := container.Get(dependency.Dependencies[i])
 		arguments = append(arguments, d)
 	}
 
-	results := dependency.callback(arguments...)
+	results := dependency.Callback(arguments...)
 
-	container.instances[dependency.name] = results
+	container.Instances[dependency.Name] = results
 
 	return results, nil
 }
 
 func (container *Container) Get(name string) (interface{}, error) {
-	if _, ok := container.dependencies[name]; !ok {
+	if _, ok := container.Dependencies[name]; !ok {
 		return nil, errors.New(fmt.Sprintf("Failed to find dependency %s", name))
 	}
 
-	return container.Inject(container.dependencies[name], false)
+	return container.Inject(container.Dependencies[name], false)
 }
 
 func (container *Container) Refresh(name string) *Container {
-	if _, ok := container.dependencies[name]; ok {
-		delete(container.dependencies, name)
+	if _, ok := container.Dependencies[name]; ok {
+		delete(container.Dependencies, name)
 	}
 	return container
 }
 
 func (container *Container) Has(name string) bool {
-	_, ok := container.dependencies[name]
+	_, ok := container.Dependencies[name]
 	return ok
 }
--- example/go.mod
+++
@@ -1,6 +0,0 @@
-module di/example
-
-go 1.22
-
-require	github.com/utopia-go/di v0.0.1
-
